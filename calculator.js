347673f45fc6602b2aa19b6a4489bd555a97d723
NamThang2512 <110403154+NamThang2512@users.noreply.github.com>
2022-08-24 20:08:47 +0700

    update

---
+++ calculator.js
@@ -0,0 +1,85 @@
+function addParenthesis_1(){
+    var addParenthesis_1 = document.getElementById('parenthesis-1').value;
+    document.getElementById('result').innerHTML = addParenthesis_1;
+}
+
+function addParenthesis_2(){
+    var addParenthesis_2 = document.getElementById('parenthesis-2').value;
+    document.getElementById('result').innerHTML = addParenthesis_2;
+}
+
+function addPercentage(){
+    var addPercentage = document.getElementById('percentage').value;
+    document.getElementById('result').innerHTML = addPercentage;
+}
+
+function addNine(){
+    var addNine = document.getElementById('nine').value;
+    document.getElementById('result').innerHTML = addNine;
+}
+
+function addEight(){
+    var addEight = document.getElementById('eight').value;
+    document.getElementById('result').innerHTML = addEight;
+}
+
+function addSeven(){
+    var addSeven = document.getElementById('seven').value;
+    document.getElementById('result').innerHTML = addSeven;
+}
+
+function addSix(){
+    var addSix = document.getElementById('six').value;
+    document.getElementById('result').innerHTML = addSix;
+}
+
+function addFive(){
+    var addFive = document.getElementById('five').value;
+    document.getElementById('result').innerHTML = addFive;
+}
+
+function addFour(){
+    var addFour = document.getElementById('four').value;
+    document.getElementById('result').innerHTML = addFour;
+}
+
+function addThree(){
+    var addThree = document.getElementById('three').value;
+    document.getElementById('result').innerHTML = addThree;
+}
+
+function addTwo(){
+    var addTwo = document.getElementById('two').value;
+    document.getElementById('result').innerHTML = addTwo;
+}
+
+function addOne(){
+    var addOne = document.getElementById('one').value;
+    document.getElementById('result').innerHTML = addOne;
+}
+
+function addZero(){
+    var addZero = document.getElementById('zero').value;
+    document.getElementById('result').innerHTML = addZero;
+}
+
+function addDivide(){
+    var addDivide = document.getElementById('divide').value;
+    document.getElementById('result').innerHTML = addDivide;
+}
+
+function addMultiple(){
+    var addMultiple = document.getElementById('multiple').value;
+    document.getElementById('result').innerHTML = addMultiple;
+}
+
+function addPlus(){
+    var addPlus = document.getElementById('plus').value;
+    document.getElementById('result').innerHTML = addPlus;
+}
+
+function addMinus(){
+    var addMinus = document.getElementById('minus').value;
+    document.getElementById('result').innerHTML = addMinus;
+}
+
